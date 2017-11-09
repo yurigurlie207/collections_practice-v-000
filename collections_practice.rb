1f747c38e73d656565b5b963e5ff500986bdce6c
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-09 20:34:50 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,75 @@
+require 'pry'
 
+def sort_array_asc(intArray)
+  intArray.sort
+end
+
+def sort_array_desc(intArray)
+  intArray.sort.reverse
+end
+
+def sort_array_char_count(strArray)
+  strArray.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  return array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  temp = array[index]
+  array[index] = array[destination_index]
+  array[destination_index] = temp
+  return array
+end
+
+def reverse_array(intArray)
+  intArray.reverse
+end
+
+
+def kesha_maker(strArray)
+  newArray = []
+  strArray.each do |string|
+    tempArray = string.split("")
+    tempArray[2] = "$"
+    newArray << tempArray.join("")
+  end
+  return newArray
+end
+
+def find_a(strArray)
+  newArray = []
+  strArray.each do |string|
+    tempArray = string.split("")
+    newArray << tempArray.join("") if tempArray.first == "a"
+  end
+  return newArray
+end
+
+def sum_array(intArray)
+  sum = 0
+  intArray.each do |number|
+    sum = sum + number
+  end
+
+  return sum
+end
+
+def add_s(strArray)
+  newArray = []
+  strArray.each do |string|
+    if string != "feet" then
+      tempArray = string.split('')
+      tempArray << "s"
+      string = tempArray.join('')
+    end
+      newArray << string
+  end
+  return newArray
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,14 @@ describe 'collections practice' do
     end
   end
 
+  # Question 4: Advanced
+  describe '#swap_elements_from_to' do
+    it 'swap the given index with destination index of an array' do
+      expect(swap_elements_from_to(["a", "b", "c"],0,2)).to eq(["c", "b", "a"])
+    end
+  end
+
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
